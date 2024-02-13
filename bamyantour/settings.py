15cd0f22139f0ee6a7419f05412341d8df350b58
ZabeenaSherzoie <zabeenasherzoie@gmail.com>
2024-02-13 06:41:47 +0000

    first deployment

--- bamyantour/settings.py
+++ bamyantour/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-zabeenasherzoie-btour-6g30bn1sdc7.ws-us108.gitpod.io']
+ALLOWED_HOSTS = ['8000-zabeenasherzoie-btour-6g30bn1sdc7.ws-us108.gitpod.io','b-tour-337c4f40df65.herokuapp.com']
 
 
 # Application definition

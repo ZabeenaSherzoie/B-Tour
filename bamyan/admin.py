2a8655bc02bd732a24b3f18eb5ab9bf0d6341e9b
ZabeenaSherzoie <zabeenasherzoie@gmail.com>
2024-02-13 07:12:15 +0000

    added modules

--- bamyan/admin.py
+++ bamyan/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from .models import TourAgency, Season, Package, Review
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
+
+
+@admin.register(TourAgency)
+class TourAgencyAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description')
+
+
+@admin.register(Package)
+class PackageAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'duration', 'slug', 'group_size')
+    search_fields = ('title', 'season')
+    list_filter = ('season',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description', 'note')
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'package', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('name', 'email', 'body')
+
+
+admin.site.register(Season)
--- bamyan/models.py
+++ bamyan/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class TourAgency(models.Model):
+    """
+    Tour agency model to add information about the agency, give
+    description of its services and its location.
+    """
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=20)
+    description = models.TextField(
+        blank=True, help_text='Warning editing this'
+        'field will change the About Us section on the home page!')
+
+    def __str__(self):
+        return self.name
+
+
+class Season(models.Model):
+    """
+    Season model to add the name of season in
+    which the packages will be available, and the information about when
+    a season starts and ends.
+    """
+    name = models.CharField(max_length=10)
+    start = models.CharField(max_length=15)
+    end = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+
+class Package(models.Model):
+    """
+    Packages model to add information about packages, the seasons they
+    are available,the start place, duration etc.
+    """
+    GSIZES = ((4, "Four"), (8, "Eight"), (12, "Tweleve"), (16, "Sixteen"))
+    season = models.ForeignKey(
+        Season, on_delete=models.CASCADE, related_name='packages')
+    title = models.CharField(max_length=100)
+    featured_image = CloudinaryField('image', default='placeholder')
+    slug = models.SlugField(max_length=100, unique=True, default='')
+    duration = models.CharField(max_length=50, default="3 days")
+    start_place = models.CharField(max_length=50, default="Kabul")
+    end_place = models.CharField(max_length=50, default="Bamyan")
+    group_size = models.IntegerField(choices=GSIZES, default=4)
+    description = models.TextField(
+        blank=False, default="No description is provided!")
+    note = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    """ 
+    Reviews model to get reviews from users about packages and their experience
+    """
+    package = models.ForeignKey(
+        Package, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"Review {self.body} by {self.name}"
+
